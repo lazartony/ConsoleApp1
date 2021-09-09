446957e1ccf9d442a27cd5941f081adc36849496
Lazar Tony <lazartony00@gmail.com>
2021-09-09 16:55:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void PrintStarTriangle(int n)
+        {
+            for (int i = n; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            PrintStarTriangle(3);
+        }
+
+        
+    }
+}
